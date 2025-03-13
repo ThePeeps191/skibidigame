1a4c0c630e1d57ad57d310878efa79122c6deeff
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2025-03-13 14:59:43 -0600

    scene fade

--- Assets/SpaceKeyScript.cs
+++ Assets/SpaceKeyScript.cs
@@ -14,7 +14,7 @@ public class TitleScreenScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene("SampleScene");
+            SceneChanger.Instance.ChangeScene("SampleScene");
         }
     }
 }
