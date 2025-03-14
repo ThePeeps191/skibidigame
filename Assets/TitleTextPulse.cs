5e8c0ad177ef3da58bb86c471280a59095aba0a7
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2025-03-14 13:24:38 -0600

    small edit

--- Assets/TitleTextPulse.cs
+++ Assets/TitleTextPulse.cs
@@ -11,7 +11,7 @@ public class TitleTextPulse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float scale =  Mathf.Max(1, 1 + Mathf.Cos(Time.time * (120 / 60) * Mathf.PI * 2) / 7);
+        float scale =  Mathf.Max(1, 1 + Mathf.Cos(Time.time * (145 / 60) * Mathf.PI * 2) / 7);
         transform.localScale = new Vector3(scale, scale, scale);
     }
 }
