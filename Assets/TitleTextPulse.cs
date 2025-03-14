37c1b70c47e2e10fde5a735ade7472873aac7a9a
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2025-03-14 12:51:48 -0600

    stuff

---
+++ Assets/TitleTextPulse.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TitleTextPulse : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scale =  Mathf.Max(1, 1 + Mathf.Cos(Time.time * (120 / 60) * Mathf.PI * 2) / 7);
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+}
