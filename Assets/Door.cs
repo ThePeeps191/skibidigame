e869db779b27011132b5a3f0b52eddfcb9173396
NoxiousWarrior <noxiouswarrior@gmail.com>
2025-04-29 13:54:08 -0600

    more features and lights

---
+++ Assets/Door.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public float rotationAngle = 90f;
+    public float openSpeed = 2f;
+
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+    private bool isOpen = false;
+    private bool isAnimating = false;
+
+    void Start()
+    {
+        closedRotation = transform.rotation;
+        openRotation = Quaternion.Euler(transform.eulerAngles - new Vector3(0, rotationAngle, 0)); // ← Opposite direction
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !isAnimating)
+        {
+            isOpen = !isOpen;
+            isAnimating = true;
+        }
+
+        if (isAnimating)
+        {
+            Quaternion targetRotation = isOpen ? openRotation : closedRotation;
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * openSpeed);
+
+            if (Quaternion.Angle(transform.rotation, targetRotation) < 0.5f)
+            {
+                transform.rotation = targetRotation;
+                isAnimating = false;
+            }
+        }
+    }
+}
